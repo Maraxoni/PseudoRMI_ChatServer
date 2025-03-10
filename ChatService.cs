0820bf216e9376dd634aa94d86787649deab2b14
Maraxoni <103191241+Maraxoni@users.noreply.github.com>
2025-03-10 15:26:04 +0100

    Add project files.

---
+++ ChatService.cs
@@ -0,0 +1,36 @@
+﻿using CoreWCF;
+
+namespace PseudoRMI_ChatServer
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ChatService : IChatService
+    {
+        private List<IChatCallback> clients = new List<IChatCallback>();
+
+        public void SendMessage(string message)
+        {
+            foreach (var client in clients)
+            {
+                client.ReceiveMessage(message);
+            }
+        }
+
+        public void Subscribe()
+        {
+            IChatCallback client = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+            if (!clients.Contains(client))
+            {
+                clients.Add(client);
+            }
+        }
+
+        public void Unsubscribe()
+        {
+            IChatCallback client = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+            if (clients.Contains(client))
+            {
+                clients.Remove(client);
+            }
+        }
+    }
+}
---
+++ IChatService.cs
@@ -0,0 +1,23 @@
+﻿using CoreWCF;
+
+namespace PseudoRMI_ChatServer
+{
+    [ServiceContract(CallbackContract = typeof(IChatCallback))]
+    public interface IChatService
+    {
+        [OperationContract]
+        void SendMessage(string message);
+
+        [OperationContract]
+        void Subscribe();
+
+        [OperationContract]
+        void Unsubscribe();
+    }
+
+    public interface IChatCallback
+    {
+        [OperationContract]
+        void ReceiveMessage(string message);
+    }
+}
